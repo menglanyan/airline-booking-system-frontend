fca9097877cb98c1689d839bf40bf0b54ad033aa
menglan <menglan_yan@outlook.com>
2026-03-18 18:09:54 +0000

    fix api base url

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default class ApiService {
-  static BASE_URL = "http://localhost:8082/api";
+  static BASE_URL = "https://airline-api.menglanyan.dev/api";
 
   static generateIdempotencyKey() {
     return crypto.randomUUID();
